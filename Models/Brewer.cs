2dfadacc52ba7d1d3b380d3e778e7c65fab8d124
rogelio <rogelinmitre@gmail.com>
2024-01-17 16:54:23 -0600

    enable toggling of mobile and desktop nav menus, create BrewGrid.razor

---
+++ Models/Brewer.cs
@@ -0,0 +1,11 @@
+namespace Brewlogger-Blz.Models;
+
+public class Brewer
+{
+    public string Name { get; set; }
+    public string ImageUrl { get; set; }
+    public string PageUrl { get; set; }
+    public string Description { get; set; }
+    public int StartingDose { get; set; }
+    public int StartingRatio { get; set; }
+}
