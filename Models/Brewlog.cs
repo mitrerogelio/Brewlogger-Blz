418c0762d4a9f13db198f1985260bc7a82f8ec2f
rogelio <rogelinmitre@gmail.com>
2024-01-28 21:54:09 -0600

    POST brewlog

--- Models/Brewlog.cs
+++ Models/Brewlog.cs
@@ -5,15 +5,12 @@ namespace Brewlogger_Blz.Models;
 public class Brewlog
 {
     public int? Id { get; }
-    [Required]
-    public string CoffeeName { get; set; }
+    [Required] public string CoffeeName { get; set; }
     public int Dose { get; set; }
-    [Required]
-    public string Grind { get; set; }
+    [Required] public string Grind { get; set; }
     public int BrewRatio { get; set; }
     public string Roast { get; set; } = "";
     public string BrewerUsed { get; set; } = "";
-    [Required]
-    public int Rating { get; set; }
+    [Required] public int Rating { get; set; }
     public string? Notes { get; set; }
 }
